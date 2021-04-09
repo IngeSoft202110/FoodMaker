ee6ab382d3fa4334b8d2314d40a9cb68c1e28df1
Paula Catalina Piñeros <60183047+PaulaPineros@users.noreply.github.com>
2021-04-08 21:29:43 -0500

    Tipo

---
+++ Aplicacion/foodmakera/lib/Clases/Tipo.dart
@@ -0,0 +1,52 @@
+
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+
+class Tipo {
+  String _objectId;
+  int _id_tipo;
+  String _nombre;
+
+  String get objectId => _objectId;
+
+  String get nombre => _nombre;
+
+  set nombre(String value) {
+    _nombre = value;
+  }
+
+  int get id_tipo => _id_tipo;
+
+  set id_tipo(int value) {
+    _id_tipo = value;
+  }
+
+  set objectId(String value) {
+    _objectId = value;
+  }
+
+  Tipo(this._id_tipo, this._nombre);
+
+  Tipo.Completa(this._objectId, this._id_tipo, this._nombre);
+
+ void crearDB() async{
+    if (this.nombre.isNotEmpty){
+      var tipo = ParseObject('Tipo')
+          ..setIncrement('id_tipo', 1)
+          ..set('nombre',this.nombre);
+          var respuesta =await tipo.save();
+          if(respuesta.success){
+            tipo = respuesta.results.first;
+          }
+    }
+  }
+
+  void  actualizarDB() async {
+    if (this.nombre.isNotEmpty && this._id_tipo != null
+        && this._objectId.isNotEmpty) {
+        var tipo = ParseObject('Tipo')
+            ..objectId = this._objectId
+            ..set('nombre', this._nombre);
+        await tipo.save();
+    }
+  }
+}
