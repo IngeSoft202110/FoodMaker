9ee2355c7c5d4783f09562f223c0df2ce1faff39
JoseMarioAriasAcevedo <ariasjosem@javeriana.edu.co>
2021-04-22 21:59:46 -0500

    Log in terminado, verifica que el usuario ingresado y la clave sean correctos

--- Aplicacion/foodmakera/lib/PRegistro.dart
+++ Aplicacion/foodmakera/lib/PRegistro.dart
@@ -6,6 +6,23 @@ import 'Pantallas/ListaRecetas.dart';
 import 'Config/ClienteGraphQL.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+
+
+void conectarse() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final keyApplicationId = 'yC5PSjDttvVvIkpBOWaHUZYo6lIHxQFKxwFD6ydT';
+  final keyClientKey = 'TI3txrhBGDTlkHNtpyfdODfhoNLDcJF2wdKGfPY7';
+  final keyParseServerUrl = 'https://parseapi.back4app.com';
+
+   Parse().initialize(keyApplicationId, keyParseServerUrl,
+      clientKey: keyClientKey, debug: true);
+}
+
+
+
 
 class PRegistro extends StatefulWidget{
 
@@ -19,53 +36,150 @@ class PRegistro extends StatefulWidget{
 
 class _PRegistroState extends State <PRegistro>
 {
-
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
+  final controllerUsername = TextEditingController();
+  final controllerPassword = TextEditingController();
+  bool isLoggedIn = false;
 
   @override
-  Widget build(BuildContext context){
-    return Scaffold
-      (
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
-        title: Text(
-        'Registro',
-        textAlign: TextAlign.center,
-    ),
-    //Boton de Busqued
-    ),
-    body: Container(
-    padding: EdgeInsets.all(15.0),
-    child: Column(
-    mainAxisAlignment:  MainAxisAlignment.center,
-    children: <Widget>[
-      TextField(
-        controller: _emailController,
-        decoration: InputDecoration(
-         labelText: 'Correo Electronico',
-         icon: Icon(Icons.email)
-         ) ,
+          title: const Text('Registro'),
         ),
-      SizedBox(height:15.0 ,),
-      TextField(
-        controller: _passwordController,
-        decoration: InputDecoration(
-            labelText: 'Contraseña',
-            icon: Icon(Icons.vpn_key_outlined)
-        ) ,
-      ),
-      SizedBox(height:15.0 ,),
-      FlatButton(
-        child:Text('Iniciar Sesión'),
-        color: Colors.lightGreen,
-        textColor: Colors.black,
-        onPressed: (){
+        body: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  height: 200,
+                ),
+                Center(
+                  child: const Text('Iniciar Sesion',
+                      style:
+                      TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Center(
+                  child:
+                  const Text('User Login/Logout', style: TextStyle(fontSize: 16)),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextField(
+                  controller: controllerUsername,
+                  enabled: !isLoggedIn,
+                  keyboardType: TextInputType.text,
+                  textCapitalization: TextCapitalization.none,
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black)),
+                      labelText: 'Username'),
+                ),
 
-        },
-      )
-       ],
-      )
-     ),
+                SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  controller: controllerPassword,
+                  enabled: !isLoggedIn,
+                  obscureText: true,
+                  keyboardType: TextInputType.text,
+                  textCapitalization: TextCapitalization.none,
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black)),
+                      labelText: 'Password'),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  height: 50,
+                  child: TextButton(
+                    child: const Text('Login'),
+                    onPressed: isLoggedIn ? null : () => doUserLogin(),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  height: 50,
+                  child: TextButton(
+                    child: const Text('Logout'),
+                    onPressed: !isLoggedIn ? null : () => doUserLogout(),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  void showSuccess(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Success!"),
+          content: Text(message),
+          actions: <Widget>[
+            new FlatButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
+  }
+
+  void showError(String errorMessage) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Error!"),
+          content: Text(errorMessage),
+          actions: <Widget>[
+            new FlatButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void doUserLogin() async {
+    final username = controllerUsername.text.trim();
+    final password = controllerPassword.text.trim();
+    final user = ParseUser(username, password, null);
+    var response = await user.login();
+    if (response.success) {
+      showSuccess("User was successfully login!");
+      setState(() {
+        isLoggedIn = true;
+      });
+    } else {
+      showError(response.error.message);
+    }
+
+  }
+
+  void doUserLogout() async {
+
   }
 }
--- Aplicacion/foodmakera/lib/main.dart
+++ Aplicacion/foodmakera/lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'PPrincipal.dart';
+import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final keyApplicationId = 'yC5PSjDttvVvIkpBOWaHUZYo6lIHxQFKxwFD6ydT';
+  final keyClientKey = 'TI3txrhBGDTlkHNtpyfdODfhoNLDcJF2wdKGfPY7';
+  final keyParseServerUrl = 'https://parseapi.back4app.com';
+
+  await Parse().initialize(keyApplicationId, keyParseServerUrl,
+      clientKey: keyClientKey, debug: true);
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
